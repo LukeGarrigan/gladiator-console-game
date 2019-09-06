807bf1e3d32995ffb4afffde9371e4bb9bcb3078
LukeGarrigan <lukegarrigan8@gmail.com>
2019-09-06 16:44:44 +0100

    Added a reference in the test project to the actual app so can test against it, VSCode doesn't give intellisense though :(

--- test/gladiator-game.Test/PlayerTest.cs
+++ test/gladiator-game.Test/PlayerTest.cs
@@ -1,14 +1,43 @@
 using System;
 using Xunit;
+using GladiatorGame.Players;
 
 namespace gladiator_game.Test
 {
-    public class UnitTest1
+    public class PlayerTest
     {
-        [Fact]
-        public void Test1()
-        {
 
+        private Player player;
+
+        public PlayerTest()
+        {
+            player = new Player("Luke");
         }
+
+        [Fact]
+        public void TestPlayerName()
+        {
+            
+            Assert.Equal("Luke", player.Name);
+        }
+
+        [Fact]
+        public void TestPlayerHealth()
+        {
+            Assert.Equal(100, player.Health);
+        }
+
+        [Fact]
+        public void TestPlayerEquipWep()
+        {
+            var player = new Player("Luke");
+            var wepToSwitch = player.Inventory.Items[0];
+            
+            player.SwitchWeapon(wepToSwitch.Name);
+
+            Assert.Equal(player.Inventory.WieldedWeapon, wepToSwitch);
+        }
+
+
     }
 }
