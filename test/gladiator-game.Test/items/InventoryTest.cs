257b1886c7502d563a665be305ffba7c842e4686
LukeGarrigan <lukegarrigan8@gmail.com>
2019-09-09 11:25:01 +0100

    Have added an attribute which is nothing more than a glorified //todo but heyho

--- test/gladiator-game.Test/items/InventoryTest.cs
+++ test/gladiator-game.Test/items/InventoryTest.cs
@@ -2,6 +2,9 @@
 using Xunit;
 using GladiatorGame.Items;
 using System;
+using System.Collections.Generic;
+using GladiatorGame.Items.Weapons;
+using GladiatorGame.Items.Helmets;
 
 namespace gladiator_game.Test
 {
@@ -15,6 +18,24 @@ namespace gladiator_game.Test
             var itemToDelete = inventory.Items[0];
             inventory.DeleteItem(itemToDelete.Name);
             Assert.False(inventory.Items.Contains(itemToDelete));
+        }
+
+        [Fact]
+        public void TestGetWeaponsInInventory()
+        {   
+            List<Weapon> weapons = inventory.GetItemsOfType<Weapon>();
+            Assert.Equal(1, weapons.Count);
+            Assert.Equal("Butter Knife", weapons[0].Name);
+        }
+        
+        [Fact]
+        public void TestGetAllArmourInInventory()
+        {   
+            inventory.AddItem(new StrawChest());
+
+            List<ArmourBase> armour = inventory.GetItemsOfType<ArmourBase>();
+            Assert.Equal(1, armour.Count);
+            Assert.Equal("Straw Chest", armour[0].Name);
         }
 
     }
